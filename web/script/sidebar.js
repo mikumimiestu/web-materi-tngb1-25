e1c532d9845dce790e9fd49fc189ec94b8c4dcf8
mikumimiestu <xenzadigital@gmail.com>
2025-02-09 11:42:09 +0700

    Penambahkan materi (list, table, link, image)

--- web/script/sidebar.js
+++ web/script/sidebar.js
@@ -4,8 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
         <h2 class="text-xl font-bold mb-4">Pemrograman Web</h2>
         <ul class="space-y-2">
           <li><a href="1.pendahuluan.html" class="text-gray-300 hover:text-white"><p class="text-sm font-bold">Pendahuluan</p></a></li>
-          <li><a href="2.syntax-dasar.html" class="text-gray-300 hover:text-white"><p class="text-sm font-bold">Pengenalan Syntax</p></a></li>
+          <li><a href="2.syntax-dasar.html" class="text-gray-300 hover:text-white"><p class="text-sm font-bold">Pengenalan HTML</p></a></li>
           <li><a href="a.2.struktur-dasar.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Struktur Dasar</p></a></li>
+          <li><a href="a.3.list.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| List</p></a></li>
+          <li><a href="a.4.table.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Table</p></a></li>
+          <li><a href="a.5.link.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Link</p></a></li>
+          <li><a href="a.6.image.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Image</p></a></li>
 
           <div class="flex-1 h-0.5 bg-gray-300"></div>
 
@@ -20,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
           <li><a href="a.2.struktur-dasar.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Project akhir</p></a></li>
 
           <div class="flex-1 h-0.5 bg-gray-300"></div>
-          
+
           <li><p href="#" class="text-gray-300 hover:text-white"><p class="text-sm font-bold">Kelompok 3</p></p></li>
           <li><a href="dokumentasi-kel3.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Dokumentasi</p></a></li>
           <li><a href="a.2.struktur-dasar.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Project akhir</p></a></li>
