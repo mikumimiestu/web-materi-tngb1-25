d3798a0e0c07e9312abcec18fafa417d80e9b062
mikumimiestu <xenzadigital@gmail.com>
2025-02-11 22:16:08 +0700

    Menambahkan materi pada tipe data dan operator js

--- web/script/sidebar.js
+++ web/script/sidebar.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
           <li><a href="3.pengenalan-js.html" class="text-gray-300 hover:text-white"><p class="text-sm font-bold">Pengenalan JavaScript</p></a></li>
           <li><a href="b.2.konsep-dasar.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Konsep Dasar</p></a></li>
-          <li><a href="../../../404.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Tipe Data dan Operator</p></a></li>
+          <li><a href="b.3.tipe-data-operator.html" class="text-gray-300 hover:text-white"><p class="text-xs ml-4">| Tipe Data dan Operator</p></a></li>
 
           <div class="flex-1 h-0.5 bg-gray-300"></div>
 
